b6a838a3148c5ed9b31f19aa74b519353a00837a
Candice Tate <67024645+candicetate@users.noreply.github.com>
2021-05-09 16:09:29 -0600

    Added game component and editted App.js

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Square from './Square'; 
 
 export default class Board extends Component {
@@ -17,14 +17,14 @@ return (
     {this.renderSquare(2)}
     </div>
     <div className="border-row">
-    {this.renderSquare(0)}
-    {this.renderSquare(1)}
-    {this.renderSquare(2)}
+    {this.renderSquare(3)}
+    {this.renderSquare(4)}
+    {this.renderSquare(5)}
     </div>
     <div className="border-row">
-    {this.renderSquare(0)}
-    {this.renderSquare(1)}
-    {this.renderSquare(2)}
+    {this.renderSquare(6)}
+    {this.renderSquare(7)}
+    {this.renderSquare(8)}
     </div>
     </div>
 )
